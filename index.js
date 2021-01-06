f2a3605a4f06a30ef65172b0911059b25e10844d
DevMeena <68868864+DevMeena@users.noreply.github.com>
2021-01-06 21:11:59 +0530

    Revert "my req"

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-
-
-
- $("#header").load("header.html");
-   $("#footer").load("footer.html");
-
+var header = $(".header"); // first navbar
+var nav = $(".sticky"); //second navbar
+var car = $(".car"); //carousal
 function inViewport($el) {
     var elH = $el.outerHeight(),
         H   = $(window).height(),
@@ -11,9 +8,6 @@ function inViewport($el) {
     return Math.max(0, t>0? Math.min(elH, H-t) : Math.min(b, H));
 }
 setInterval(function(){
-  var header = $(".header"); // first navbar
-  var nav = $(".sticky"); //second navbar
-  var car = $(".car"); //carousal
   var h = nav.height().toString();
   if(inViewport(header)==0){
     nav.addClass("fixed-top");
@@ -55,11 +49,18 @@ $('a[href$="#Modal2"]').on( "click", function() {
 });
 // MODAL FN ENDS
 
-$('.dropdown').on('show.bs.dropdown', function() {
+$(function(){
+$('.dropdown').on('show.bs.dropdown', function(e){
     $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
   });
 
-  // Add slideUp animation to Bootstrap dropdown when collapsing.
-  $('.dropdown').on('hide.bs.dropdown', function() {
-    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  // ADD SLIDEUP ANIMATION TO DROPDOWN //
+  $('.dropdown').on('hide.bs.dropdown', function(e){
+    e.preventDefault();
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(400, function(){
+    	$('.dropdown').removeClass('open');
+      	$('.dropdown').find('.dropdown-toggle').attr('aria-expanded','false');
+    });
+
   });
+});
