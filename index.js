f11c07aec328b4ce909bcf4bd8537a89a933ebf7
Prakhar <2019UEE1465@mnit.ac.in>
2021-01-06 19:21:13 +0530

    still testing

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var header = $(".header"); // first navbar
-var nav = $(".sticky"); //second navbar
-var car = $(".car"); //carousal
+
 
 
 $("#header").load("header.html");
-  $("#footer").load("footer.html"); 
+  $("#footer").load("footer.html");
 
-
+  var header = $(".header"); // first navbar
+  var nav = $(".sticky"); //second navbar
+  var car = $(".car"); //carousal
 function inViewport($el) {
     var elH = $el.outerHeight(),
         H   = $(window).height(),
