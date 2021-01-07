d0326f20f697cd51467e55c89b5f4987773f5cfe
DevMeena <68868864+DevMeena@users.noreply.github.com>
2021-01-07 15:02:17 +0530

    changes by pointer d's file

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ function inViewport($el) {
         r   = $el[0].getBoundingClientRect(), t=r.top, b=r.bottom;
     return Math.max(0, t>0? Math.min(elH, H-t) : Math.min(b, H));
 }
-var header = $(".header"); // first navbar
-var nav = $(".sticky"); //second navbar
-var car = $(".car"); //carousal
+
 function inViewport($el) {
+
     var elH = $el.outerHeight(),
         H   = $(window).height(),
         r   = $el[0].getBoundingClientRect(), t=r.top, b=r.bottom;
     return Math.max(0, t>0? Math.min(elH, H-t) : Math.min(b, H));
 }
 setInterval(function(){
+  var header = $(".header"); // first navbarz
+  var nav = $(".sticky"); //second navbar
+  var car = $(".car"); //carousal
   var h = nav.height().toString();
   if(inViewport(header)==0){
     nav.addClass("fixed-top");
@@ -68,32 +70,3 @@ setTimeout(function(){
       $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
 });
 }, 1000);
-
-$(function(){
-$('.dropdown').on('show.bs.dropdown', function(e){
-    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-  });
-
-  // ADD SLIDEUP ANIMATION TO DROPDOWN //
-  $('.dropdown').on('hide.bs.dropdown', function(e){
-    e.preventDefault();
-    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(400, function(){
-    	$('.dropdown').removeClass('open');
-      	$('.dropdown').find('.dropdown-toggle').attr('aria-expanded','false');
-    });
-
-  });
-});
-setInterval(function(){
-  if ($(".dropdown")[0]){
-    $('.dropdown').on('show.bs.dropdown', function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-      });
-
-      // Add slideUp animation to Bootstrap dropdown when collapsing.
-      $('.dropdown').on('hide.bs.dropdown', function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-      });
-} 
-}, 100);
-
